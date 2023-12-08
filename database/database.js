25180093d60a03709d2d5462c77c1c4fbb16ac88
Alejandro Loayza <alexloayza13@gmail.com>
2023-12-08 13:23:49 +0100

    .

--- database/database.js
+++ database/database.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect('mongodb://127.0.0.1:27017/reservas').then(db => console.log('Database connected')).catch((error) => console.error(error));
+mongoose.connect(process.env.MONGODB_URI).then(db => console.log('Database connected')).catch((error) => console.error(error));
+
+//*'mongodb://127.0.0.1:27017/reservas'
